5e3baccd8ad0a3199cb872342d48d9e537e8c23b
edwardsonjennifer <79241050+edwardsonjennifer@users.noreply.github.com>
2021-02-23 23:58:08 -0800

    Edited firstProgram and added 01_random

--- firstProgram.py
+++ firstProgram.py
@@ -1,3 +1,14 @@
-name = 'Jennifer Edwardson'
-print(f'Hello {name}')
 
+
+if __name__== "__main__":
+    # Created by James Mertz
+    with open('01_random.txt') as reader:
+        counts = dict()
+        for line in reader:
+            words = line.split()
+            for word in words:
+                counts[word] = counts.get(word, 0) + 1
+                
+    # Get the max value based on the key's value
+    most_used = max(counts, key=lambda key: counts[key])
+    print(f"The most used word is '{most_used}' appearing {counts[most_used]} times.")
