01565a29c61e4ebcd6e243ea4dc97879f16fa742
edwardsonjennifer <79241050+edwardsonjennifer@users.noreply.github.com>
2021-03-18 19:40:58 -0700

    Numbers and Operators
    Module 4

---
+++ .vscode/Module 4/numbers.py
@@ -0,0 +1,56 @@
+
+
+if __name__ == "__main__":
+
+    # age = 32
+    # print(age)
+    # print(type(age))
+
+    # str_age = '32'
+    # age = int(str_age)
+    # print(age)
+    # print(type(age))
+
+    # age_bin_str = '100000'
+    # age = int(age_bin_str, 2)
+    # print(age)
+    # print(type(age))
+
+    # # F = 15 FF = 255 A = 10
+    # age_hex_str = 'FACE' #64206
+    # age = int(age_hex_str, 16)
+    # print(age)
+    # print(type(age))
+
+    # one_billion = 1_000_000_000
+    # print(one_billion)
+
+    # changing from float to string to float
+    # age = 32.3
+    # age = '32.3'
+    # age = float('32.3')
+
+    # one_fillion = 1_000_000_000.0
+
+    # # 1e12 or (1*10)^12
+    # one_trillion = 1e12
+    # print(one_trillion)
+
+    # really_small_num = 3.14e-12
+    # print(really_small_num)
+
+    # #beyond what python can calculate 2e400/-2e400
+    # too_big = -2e400
+    # print(too_big)
+
+    # # GOTCHA equals 0.30000000000000004
+    # float_val = .1 + .1 + .1
+    # print(float_val)
+
+    # j is notation for complex numbers, square root of -1
+    my_complex_num = 2 + 3j
+    print(my_complex_num.real)
+    print(my_complex_num.imag)
+    print(my_complex_num.conjugate())
+
+    print('script complete')
---
+++ .vscode/Module 4/numerical_operators.py
@@ -0,0 +1,31 @@
+
+if __name__ == "__main__":
+    
+    # Addition
+    result = 3 + 9
+    result = -3 + 4.0
+
+    # Subtraction
+    result = -3 - 4.0
+    result = -10.0 - 4
+
+    # Multiplication
+    result = 4 * 3
+    result = 6 * -2.1
+
+    # Division will do floating point by default
+    result = 4 / 2
+    result = 8 / 3.0
+
+    # Integer Division will do the integer
+    result = 5 // 3
+
+    # Modulus (Remainder)
+    remainder = 5 % 3
+
+    # Exponentiation
+    result = 2 ** 3
+    result = 9 ** 0.5
+    result = 27 ** (1 / 3)
+
+    print('script complete')
