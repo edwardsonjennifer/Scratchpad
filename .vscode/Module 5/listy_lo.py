f7b949fa9f83563b4a63ffbf6bec6005a5a8d833
edwardsonjennifer <79241050+edwardsonjennifer@users.noreply.github.com>
2021-03-25 21:28:14 -0700

    Added listy_lo.py

--- .vscode/Module 5/listy_lo.py
+++ .vscode/Module 5/listy_lo.py
@@ -1,23 +1,27 @@
 if __name__ == "__main__":
-    # food = ['rice', 'beans']
-    # print(food)
+    food = ['rice', 'beans']
+    print(food)
 
-    # food.append('broccoli')
-    # print(food)
+    food.append('broccoli')
+    print(food)
 
-    # food.extend(['bread', 'pizza'])
-    # print(food)
+    food.extend(['bread', 'pizza'])
+    print(food)
 
-    # print(food[len(food) - 1])
+    print(food[len(food) - 1])
 
-    # morning = ('eggs, fruit, orange juice')
-    # breakfast = morning.split(',')
-    # print(breakfast)
-    # print(len(breakfast))
+    morning = ('eggs, fruit, orange juice')
+    breakfast = morning.split(',')
+    print(breakfast)
+    print(len(breakfast))
  
+    numlist = []
+    while True:
+        inp = input('Enter an number(type "stop" to end): ')
+        if inp == 'stop':
+            break
+        value = float(inp)
+        numlist.append(value)
 
-
-
-
-
-    print('script compleate')
+    fl_avg = sum(numlist) / len(numlist)
+    print(f'Average: {fl_avg}, Min: {min(numlist)}, Max: {max(numlist)}')
