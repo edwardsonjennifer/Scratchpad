4468651815ff9177ba879fa1ab934ca49b31490b
edwardsonjennifer <79241050+edwardsonjennifer@users.noreply.github.com>
2021-05-24 02:01:10 -0700

    Create pokemon_oop.py

---
+++ .vscode/Module_9/pokemon_oop.py
@@ -0,0 +1,10 @@
+caterpie = ['caterpie', 13, 40, ('String Shot', 10)]
+
+print (caterpie[0])
+
+caterpie = {
+    'name': 'caterpie',
+    'level': 13,
+    'hp': 40,
+    'move': ('String Shot', 10)
+}
