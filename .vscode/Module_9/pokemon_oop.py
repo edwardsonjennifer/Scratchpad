cbf2d5421eab21241c48e96788d5212eb18e9048
edwardsonjennifer <79241050+edwardsonjennifer@users.noreply.github.com>
2021-05-24 18:00:31 -0700

    Update pokemon_oop.py

--- .vscode/Module_9/pokemon_oop.py
+++ .vscode/Module_9/pokemon_oop.py
@@ -8,3 +8,31 @@ caterpie = {
     'hp': 40,
     'move': ('String Shot', 10)
 }
+### Module 01 Notes
+## What is OOP?
+# sometimes code can repetitive or weird, wanting a neat and clean code
+# it gives you the ability to distile a massive amount of code into a section
+
+## OOP is a paradigm
+# using objects to represent specific elements within a program
+# objects have data, functions, etc. OOP allows us to hide complexity
+# good philosiphy is to break down code to it's smallest elements and breach out from there
+
+## OOP is not new
+# all the way back to 1950 to 1960. Simula was in the early 1970
+# Today, we have programs that are built around OOP (Python, Java, C#)
+# and implemented aspects of OOP (PHP, Perl, or Matlab)
+
+## OOP has 4 concepts
+# -Encapsulation - the processd of encapsulating, or organizing code so that only the relevant
+# parts are available to the user.
+# -Abstraction - the process of abstracting, or templating code to show the basic requirements
+# a specific set of code should accomplish. 
+# -Inheritance - the process of inheriting, or reusing code with little to no changes in between
+# objects.
+# -Polymorphism - the concept that one thing, a class, can be used to decribe multiple different
+# things even though the details may be different for each.
+
+# An object like Pokemon Cards can have similar attributes (name, hp, type, move(s), weakness)
+# Pokemon can be thought as a class of monsters. One specific pokemon can be a object or instance
+# of the class Pokemon. ##
